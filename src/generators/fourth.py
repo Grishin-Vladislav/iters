a029853613926fa6f3293d57a68ecda45b589b16
GVladislav <grishinvladislav97@gmail.com>
2023-06-21 15:02:14 +0300

    Add generators

--- src/generators/fourth.py
+++ src/generators/fourth.py
@@ -1,2 +1,6 @@
-def flat_generator2(list_of_list):
-    pass
+def flat_generator2(list_of_lists):
+    for pos, item in enumerate(list_of_lists):
+        if type(item) is list:
+            yield from flat_generator2(item)
+        else:
+            yield item
